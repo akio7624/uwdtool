f5e15fe4ede911395978ee27fc882affd6fa6b17
akio7624 <97262588+akio7624@users.noreply.github.com>
2025-04-09 16:00:49 +0900

    add Common.py

---
+++ uwdtool/Common.py
@@ -0,0 +1,17 @@
+UWDT_VERSION = "1.1.0"
+HELP_STR = f"""UWDTool v{UWDT_VERSION}"""
+
+def print_err(msg: str):
+    print(msg)
+    print(f"exit program...")
+    exit(1)
+
+def sizeof_fmt(size: int, suffix="B"):
+    for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
+        if abs(size) < 1024.0:
+            return f"{size:3.1f}{unit}{suffix}"
+        size /= 1024.0
+    return f"{size:.1f}Yi{suffix}"
+
+def to_hex(n: int, digit: int) -> str:
+    return f"0x{n:0{digit}X}"
