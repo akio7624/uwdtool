3317c53018e6f4ffcd2bb30911ba7c9c2a9782c2
akio7624 <97262588+akio7624@users.noreply.github.com>
2025-04-16 15:26:13 +0900

    add blank line (PEP 8: E302)

--- uwdtool/Common.py
+++ uwdtool/Common.py
@@ -14,10 +14,12 @@ UWDTool v{UWDT_VERSION}
   If -o is not specified, the files will be saved in a folder named output under the current working directory.
 """.strip()
 
+
 def print_err(msg: str):
     print(msg)
     print(f"exit program...")
     exit(1)
+
 
 def sizeof_fmt(size: int, suffix="B") -> str:
     for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
@@ -25,6 +27,7 @@ def sizeof_fmt(size: int, suffix="B") -> str:
             return f"{size:3.1f}{unit}{suffix}"
         size /= 1024.0
     return f"{size:.1f}Yi{suffix}"
+
 
 def to_hex(n: int, digit: int) -> str:
     return f"0x{n:0{digit}X}"
